2d38ecc636e3e5c8b2a53df6567c5638239f59ad
Austin Grech <grech.austin@yahoo.ca>
2023-07-30 08:51:34 -0400

    added atlas db to mongoose connection path

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://austingrech:austingrech1@cluster0.6kmy6bx.mongodb.net/googlebooks"
+);
 
 module.exports = mongoose.connection;
